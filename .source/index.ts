e5449f7a1930b5a5b353e77dd6991f8ef9c1533e
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-05-08 20:16:14 +0530

    feat: add initial source configuration and documentation setup

---
+++ .source/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck -- skip type checking
+import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1746020618333"
+import * as docs_0 from "../content/docs/introduction.mdx?collection=docs&hash=1746020618333"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"introduction.mdx","absolutePath":"C:/Users/SUBHADEEP ROY/Desktop/networking/content/docs/introduction.mdx"}, data: docs_0 }]);
+export const meta = _runtime.meta<typeof _source.meta>([{ info: {"path":"meta.json","absolutePath":"C:/Users/SUBHADEEP ROY/Desktop/networking/content/docs/meta.json"}, data: meta_0 }]);
