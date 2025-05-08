e38d2aad40aca51fb2e723fd418235f40e27895a
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-05-08 20:17:01 +0530

    feat: implement new documentation page with dynamic content and metadata generation

---
+++ app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,57 @@
+import { source } from "@/lib/source";
+import {
+  DocsPage,
+  DocsBody,
+  DocsDescription,
+  DocsTitle,
+} from "fumadocs-ui/page";
+import { notFound } from "next/navigation";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import { metadataImage } from "@/lib/metadata";
+
+export default async function Page(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  const MDX = page.data.body;
+
+  // const lastModified = await getLastModified(page);
+
+  return (
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      // lastUpdate={lastModified ? new Date(lastModified) : undefined}
+    >
+      <DocsTitle>{page.data.title}</DocsTitle>
+      <DocsDescription>{page.data.description}</DocsDescription>
+      <DocsBody className="prose-img:invert dark:prose-img:invert-0">
+        <MDX
+          components={{
+            ...defaultMdxComponents
+          }}
+        />
+      </DocsBody>
+    </DocsPage>
+  );
+}
+
+export async function generateStaticParams() {
+  return source.generateParams();
+}
+
+export async function generateMetadata(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  return metadataImage.withImage(page.slugs, {
+    title: page.data.title,
+    description: page.data.description,
+  });
+}
